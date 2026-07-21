6aa02487ad9e3dd385f009fa76d4e3b64b27d95b
John Parkhurst <jparkhurst120@gmail.com>
2026-07-21 10:29:46 -0400

    Update Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from "@emotion/styled";
+
+
+const PageContainer = styled("div")({
+  display: "grid",
+  gridTemplateColumns: "24% 50% 24%",
+  padding: "10px",
+  gap: "1%",
+});
+
 
 export const Profile = () => {
   return (
-    <div>
-      Profile!
-    </div>
+    <PageContainer>
+      <p>Pro</p>
+
+    </PageContainer>
   )
 }
